2acca4e30f147d44b9f5824bfb221672fbbd9103
Craig Rees <craig.rees@iblocks.co.uk>
2025-12-16 17:19:36 +0000

    Test

--- android/src/main/kotlin/com/example/hpp_flutter/HppFlutterPlugin.kt
+++ android/src/main/kotlin/com/example/hpp_flutter/HppFlutterPlugin.kt
@@ -7,6 +7,9 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.annotation.NonNull
+import com.google.gson.Gson
+import com.google.gson.annotations.SerializedName
+import com.google.gson.reflect.TypeToken
 import com.realexpayments.hpp.HPPManager
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
@@ -56,17 +59,13 @@ class HppFlutterPlugin :
 
                 // validate
                 if (data == null
-                        || data.hppURL.isEmpty()
-                        || data.hppRequestConsumerURL.isEmpty()
-                        || data.hppRequestProducerURL.isEmpty()) {
+                        || data.hppURL.isEmpty()) {
                     result.success(mutableMapOf(RESULT_FIELD_SUCCESS to false, RESULT_FIELD_RESULT to "No or invalid configuration provided", RESULT_FIELD_CODE to 9999))
                     return
                 }
 
                 val intent = Intent(context, PaymentActivity::class.java)
                 val bundle = Bundle()
-                bundle.putString(HPPManager.HPPREQUEST_PRODUCER_URL, data.hppRequestProducerURL)
-                bundle.putString(HPPManager.HPPRESPONSE_CONSUMER_URL, data.hppRequestConsumerURL)
                 bundle.putString(HPPManager.HPPURL, data.hppURL)
 
                 intent.putExtra("hppData", bundle)
@@ -83,7 +82,7 @@ class HppFlutterPlugin :
         channel.setMethodCallHandler(null)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?): Boolean {
+    fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?): Boolean {
         return when (requestCode) {
             PAYMENT_REQUEST_CODE -> handleResult(resultCode, data)
             else -> return false
@@ -100,12 +99,28 @@ class HppFlutterPlugin :
     }
 }
 
-data class PayloadData(@SerializedName("HPPRequestProducerURL") val hppRequestProducerURL: String,
-                       @SerializedName("HPPResponseConsumerURL") val hppRequestConsumerURL: String,
-                       @SerializedName("HPPURL") val hppURL: String
+data class PayloadData(@SerializedName("HPPURL") val hppURL: String
 )
 
 fun ArrayList<Any>?.asPayloadData(): PayloadData? {
-    val data: HashMap<String, Any>? = this[0] as HashMap<String, Any>
+    val data: HashMap<String, Any> = this[0] as HashMap<String, Any>
     return data?.toDataClass<PayloadData>()
+}
+
+val gson = Gson()
+
+//convert a data class to a map
+fun <T> T.serializeToMap(): Map<String, Any> {
+    return convert()
+}
+
+//convert a map to a data class
+inline fun <reified T> Map<String, Any>.toDataClass(): T {
+    return convert()
+}
+
+//convert an object of type I to type O
+inline fun <I, reified O> I.convert(): O {
+    val json = gson.toJson(this)
+    return gson.fromJson(json, object : TypeToken<O>() {}.type)
 }
