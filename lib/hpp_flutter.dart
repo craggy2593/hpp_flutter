fc1620d3f057f222b91967f02658305f4f6b06fb
Craig Rees <craig.rees@iblocks.co.uk>
2025-12-16 12:40:08 +0000

    Tweaks

--- lib/hpp_flutter.dart
+++ lib/hpp_flutter.dart
@@ -1,8 +1,34 @@
+import 'dart:async';
 
-import 'hpp_flutter_platform_interface.dart';
+import 'package:flutter/services.dart';
+
+class HppResult {
+  final bool success;
+  final String result;
+  final int code;
+
+  HppResult(this.success, this.result, this.code);
+}
 
 class HppFlutter {
-  Future<String?> getPlatformVersion() {
-    return HppFlutterPlatform.instance.getPlatformVersion();
+  static const MethodChannel _channel = const MethodChannel('hpp_flutter');
+
+  static Future<HppResult> showPaymentWindow({
+    String hppRequestProducerURL,
+    String hppResponseConsumerURL,
+    String hppURL,
+  }) async {
+    final payload = {
+      'HPPRequestProducerURL': hppRequestProducerURL,
+      'HPPResponseConsumerURL': hppResponseConsumerURL,
+      'HPPURL': hppURL,
+    };
+
+    final Map<dynamic, dynamic> result = await _channel.invokeMethod(
+      'showPaymentWindow',
+      [payload],
+    );
+    var result = HppResult(result['success'], result['result'], result['code']);
+    return result;
   }
 }
